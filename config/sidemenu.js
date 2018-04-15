7620af56b4b7e72893ba3ff7e9c78c5c9a5207b7
sober_e1@qq.com <sober_e1@qq.com>
2018-04-15 10:43:34 +0800

    首页

---
+++ config/sidemenu.js
@@ -0,0 +1,31 @@
+var dataArray = [
+    [{
+        'item': '公告首页',
+        'href': 'pages/annoumcement/index_announce.html',
+    }, {
+        'item': '发布公告',
+        'href': 'pages/annoumcement/push_announce.html',
+    }, {
+        'item': '公告查询',
+        'href': 'pages/annoumcement/query_announce.html',
+    }],
+    [{
+        'item': '发布培训任务',
+        'href': 'pages/train/push_train.html',
+    }, {
+        'item': '培训任务查询',
+        'href': 'pages/train/query_train.html',
+    }, {
+        'item': '学习资料库',
+        'href': 'pages/train/library.html',
+    }, {
+        'item': '发布考试任务',
+        'href': 'pages/train/push_exam.html',
+    }, {
+        'item': '考试影像分析',
+        'href': 'pages/train/analysis_video.html',
+    }, {
+        'item': '调查问卷管理',
+        'href': 'pages/train/questionnaire.html',
+    }]
+]
