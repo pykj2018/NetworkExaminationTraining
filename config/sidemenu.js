49866e35268c00fef5f0f3719558d03c095ab79f
siyu.liu <765500814@qq.com>
2018-04-23 15:46:21 +0800

    动态渲染侧边列表栏

--- config/sidemenu.js
+++ config/sidemenu.js
@@ -2,30 +2,42 @@ var dataArray = [
     [{
         'item': '公告首页',
         'href': 'pages/annoumcement/index_announce.html',
+        'submenu': []
     }, {
         'item': '发布公告',
         'href': 'pages/annoumcement/push_announce.html',
+        'submenu': []
     }, {
         'item': '公告查询',
         'href': 'pages/annoumcement/query_announce.html',
+        'submenu': []
     }],
     [{
         'item': '发布培训任务',
         'href': 'pages/train/push_train.html',
+        'submenu': [
+            '查询',
+            '搜索'
+        ]
     }, {
         'item': '培训任务查询',
         'href': 'pages/train/query_train.html',
+        'submenu': []
     }, {
         'item': '学习资料库',
         'href': 'pages/train/library.html',
+        'submenu': []
     }, {
         'item': '发布考试任务',
         'href': 'pages/train/push_exam.html',
+        'submenu': []
     }, {
         'item': '考试影像分析',
         'href': 'pages/train/analysis_video.html',
+        'submenu': []
     }, {
         'item': '调查问卷管理',
         'href': 'pages/train/questionnaire.html',
+        'submenu': []
     }]
 ]
