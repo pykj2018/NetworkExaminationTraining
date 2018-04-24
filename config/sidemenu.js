5b9774995d37a9655f3107236df717760a460630
sober_e1@qq.com <sober_e1@qq.com>
2018-04-24 10:23:26 +0800

    用户弹层

--- config/sidemenu.js
+++ config/sidemenu.js
@@ -15,13 +15,13 @@ var dataArray = [
     [{
         'item': '发布培训任务',
         'submenu': [{
-                'xitem': '查询',
-                'xhref': 'pages/train/push_train.html'
-            },
-            {
-                'xitem': '搜索',
-                'xhref': 'pages/train/push_train.html'
-            }
+            'xitem': '查询',
+            'xhref': 'pages/train/push_train.html'
+        },
+        {
+            'xitem': '搜索',
+            'xhref': 'pages/train/push_train.html'
+        }
         ]
     }, {
         'item': '培训任务查询',
@@ -30,22 +30,30 @@ var dataArray = [
     }, {
         'item': '学习资料库',
         'href': 'pages/train/library.html',
-        'submenu': []
+        'submenu': [{
+            'xitem': '添加资料',
+            'xhref': 'pages/train/libraryUpload.html',
+        },
+        {
+            'xitem': '查看资料',
+            'xhref': 'pages/train/library.html'
+        }
+        ]
     }, {
         'item': '发布考试任务',
         'href': 'pages/train/push_exam.html',
         'submenu': [{
-                'xitem': '发布',
-                'xhref': 'pages/train/push_exam.html'
-            },
-            {
-                'xitem': '发布1',
-                'xhref': 'pages/train/push_exam.1.html'
-            },
-            {
-                'xitem': '发布2',
-                'xhref': 'pages/train/push_exam.2.html'
-            }
+            'xitem': '发布',
+            'xhref': 'pages/train/push_exam.html'
+        },
+        {
+            'xitem': '发布1',
+            'xhref': 'pages/train/push_exam.1.html'
+        },
+        {
+            'xitem': '发布2',
+            'xhref': 'pages/train/push_exam.2.html'
+        }
         ]
     }, {
         'item': '考试影像分析',
@@ -53,23 +61,19 @@ var dataArray = [
         'submenu': []
     }, {
         'item': '调查问卷管理',
-        'href': 'pages/train/questionnaire.html',
-        'submenu': [],
-    }, {
-        'item': '资料添加管理',
         'href': 'pages/train/libraryUpload.html',
         'submenu': [{
-                'xitem': '调查问卷添加',
-                'xhref': 'pages/train/questionnaire.html',
-            },
-            {
-                'xitem': '调查问卷查询',
-                'xhref': 'pages/train/questionnaire1.html'
-            },
-            {
-                'xitem': '调查问卷分析',
-                'xhref': 'pages/train/questionnaire2.html'
-            }
+            'xitem': '调查问卷添加',
+            'xhref': 'pages/train/questionnaire.html',
+        },
+        {
+            'xitem': '调查问卷查询',
+            'xhref': 'pages/train/questionnaire1.html'
+        },
+        {
+            'xitem': '调查问卷分析',
+            'xhref': 'pages/train/questionnaire2.html'
+        }
         ]
     }]
 ]
