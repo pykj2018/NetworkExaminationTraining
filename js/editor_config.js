078c41867a2b548f90c1c4b8b5fb2ecb55926961
sober_e1@qq.com <sober_e1@qq.com>
2018-05-09 12:46:14 +0800

    解決多個富文本無法共存

--- js/editor_config.js
+++ js/editor_config.js
@@ -4,26 +4,20 @@
  * @method init 初始化
  * @method getContent 获取富文本内容
  */
-var editorConfig = {
-    config: null,
-    E: window.wangEditor,
-    editor: null,
-    _create: function (config) {
-        var editor = this.editor;
-        editor = new this.E(config);
-        // editor.customConfig.uploadImgServer = '/upload'
-        editor.customConfig.zIndex = 1;
-        editor.customConfig.height = '150';
-        editor.customConfig.uploadImgShowBase64 = true;
-        editor.create();
-    },
-    init: function (config) {
-        this._create(config);
-        return this;
-    },
-    getContent: function () {
+function EditorConfig(config) {
+    this.config = config;
+    this.E = window.wangEditor;
+    this.editor = new this.E(config);
+    this._create = function () {
+        // this.editor.customConfig.uploadImgServer = '/upload'
+        this.editor.customConfig.zIndex = 1;
+        this.editor.customConfig.height = '150';
+        this.editor.customConfig.uploadImgShowBase64 = true;
+        this.editor.create();
+    }
+    this.getContent = function () {
         var con = this.editor.txt.html();
-        console.log(con);
+        return con;
     }
 }
 
